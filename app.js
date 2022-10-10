11828683937e8774170a767cd0478e774aeaba5c
Erick Luciano <erickhbd@gmail.com>
2022-10-10 11:38:17 -0300

    foram adicionadsa as imagens e os áudios nas respostas, porém as imagens ainda não funcionaram.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var numeroSecreto = parseInt(Math.random() * 11);
 
+let errou=document.getElementById("audioErrou");
+let acertou=document.getElementById("audioAcertou");
+let gameOver=document.getElementById("gameOver");
+
 var tentativas = 1;
 const totalTentativas = 3;
 
@@ -8,21 +12,28 @@ function Chutar() {
   var chute = parseInt(document.getElementById("valor").value);
 
   console.log(chute);
+
   if (chute == numeroSecreto) {
     elementoResultado.innerHTML = "Você acertou!";
+    document.getElementById('gifSilvio').style.display = "block";
+    acertou.play();
+    
   } else if (chute < 0 || chute > 10) {
     elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10.";
   } else if (tentativas >= totalTentativas) {
     elementoResultado.innerHTML = "Game Over. As tentativas acabaram. O número secreto era " + numeroSecreto + ".";
+    gameOver.play();
   } else {
     if (chute < numeroSecreto){
     elementoResultado.innerHTML =
       "errouuuuuu! O nº SECRETO é maior."
       tentativas++
+      document.getElementById('gifFausto').style.display = "block";
+    errou.play();
     } else {
       elementoResultado.innerHTML =
         "errouuuuuu! O nº SECRETO é menor."
         tentativas++
     }
-  }
+}
 }
